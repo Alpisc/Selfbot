726afb2b9a98c2ea63f5a1ec1c0bab6a2dc4e416
Alpisc <alpisc@duck.com>
2022-11-29 16:20:49 +0100

    added Update to menu

--- Menu.bat
+++ Menu.bat
@@ -3,9 +3,10 @@
 echo Selection.
 echo:
 echo (1) Selfbot
-echo (2) Setup
-echo (3) Dependencies
-echo (4) Exit
+echo (2) Update
+echo (3) Setup
+echo (4) Dependencies
+echo (5) Exit
 set /p selection=choice: 
 echo choice is: %selection%
 echo:
@@ -14,10 +15,12 @@ if %selection% NEQ 1 (
     if %selection% NEQ 2 (
         if %selection% NEQ 3 (
             if %selection% NEQ 4 (
-                cls 
-                echo "%selection%" is an invalid choice
-                echo:
-                goto :A
+                if %selection% NEQ 5 (
+                    cls 
+                    echo "%selection%" is an invalid choice
+                    echo:
+                    goto :A
+                )
             )
         )
     )
@@ -28,26 +31,36 @@ if %selection% EQU 1 (
     python3 .content/selfbot.py
 ) else (
     if %selection% EQU 2 (
-        echo Starting Setup
-        python3 .content/setup_wizard.py
+        echo Pulling newest version ...
+        git pull
         echo:
-        echo Setup finished
+        echo Finished
         pause
         cls
         goto :A
     ) else (
         if %selection% EQU 3 (
-            echo Installing dependencies
-            pip install -r .content/requirements.txt
+            echo Starting Setup
+            python3 .content/setup_wizard.py
             echo:
-            echo Finished installing dependencies
+            echo Setup finished
             pause
             cls
             goto :A
         ) else (
             if %selection% EQU 4 (
-                echo Exiting ...
-                exit
+                echo Installing dependencies
+                pip install -r .content/requirements.txt
+                echo:
+                echo Finished installing dependencies
+                pause
+                cls
+                goto :A
+            ) else (
+                if %selection% EQU 5 (
+                    echo Exiting ...
+                    exit
+                )
             )
         )
     )
